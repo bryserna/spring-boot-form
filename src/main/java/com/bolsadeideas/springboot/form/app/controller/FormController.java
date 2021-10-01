7b5c290341496551ae1c15ec653eedaed78d1a5d
bryserna <bryserna@bancolombia.com.co>
2021-10-01 16:25:04 -0500

    Manejo de errores notación @Valid - BindingResult - @NotEmpty

--- src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -3,9 +3,14 @@ package com.bolsadeideas.springboot.form.app.controller;
 import com.bolsadeideas.springboot.form.app.model.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -17,9 +22,19 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model){
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
 
         model.addAttribute("titulo", "Resultado form");
+
+        if(result.hasErrors()){
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err ->{
+                errores.put(err.getField(),"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
+
         model.addAttribute("usuario", usuario);
         return "resultado";
     }
--- src/main/java/com/bolsadeideas/springboot/form/app/model/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/model/domain/Usuario.java
@@ -1,9 +1,14 @@
 package com.bolsadeideas.springboot.form.app.model.domain;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
 
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String email;
 
     public String getUsername() {
