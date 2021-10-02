1e81a3fd2e8f7da307e8e4b28071e05afcdfd17b
bryserna <bryserna@bancolombia.com.co>
2021-10-02 10:38:12 -0500

    validaciones con @size @email

--- src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -7,24 +7,30 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
         Usuario usuario = new Usuario();
+        usuario.setNombre("Bryan");
+        usuario.setApellido("seros");
+        usuario.setIdentificador("12345-k");
         model.addAttribute("titulo","Formulario Usuario");
         model.addAttribute("usuario",usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status){
 
         model.addAttribute("titulo", "Resultado form");
 
@@ -38,6 +44,7 @@ public class FormController {
         }
 
         model.addAttribute("usuario", usuario);
+        status.setComplete();
         return "resultado";
     }
 }
--- src/main/java/com/bolsadeideas/springboot/form/app/model/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/model/domain/Usuario.java
@@ -1,14 +1,23 @@
 package com.bolsadeideas.springboot.form.app.model.domain;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 public class Usuario {
 
+    private String identificador;
     @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
+    @NotEmpty(message = "No puede ser vacio")
+    @Size(min = 3, max = 8)
     private String username;
     @NotEmpty
     private String password;
     @NotEmpty
+    @Email
     private String email;
 
     public String getUsername() {
@@ -33,5 +42,29 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
     }
 }
